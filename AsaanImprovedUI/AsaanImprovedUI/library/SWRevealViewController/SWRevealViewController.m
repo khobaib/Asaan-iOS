55cd653d1038271538ab0a78d46a06ea4c7d4929
Hasan Ibna Akbar <bd.pothik@gmail.com>
2015-01-23 23:01:57 +0600

    fix a bug

--- AsaanImprovedUI/AsaanImprovedUI/library/SWRevealViewController/SWRevealViewController.m
+++ AsaanImprovedUI/AsaanImprovedUI/library/SWRevealViewController/SWRevealViewController.m
@@ -1699,7 +1699,7 @@ const int FrontViewPositionNone = 0xff;
         
         @try
         {
-            [self performSegueWithIdentifier:SWSegueRightIdentifier sender:nil];
+//            [self performSegueWithIdentifier:SWSegueRightIdentifier sender:nil];
         }
         @catch(NSException *exception) {}
     }
