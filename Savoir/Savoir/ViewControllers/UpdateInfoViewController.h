61c603247586a51cb1150e7ff3d0d460ed0a953b
Ehtesham Hasan <mr746866@gmail.com>
2015-03-25 18:54:28 +0600

    Minor
    Minor

--- Savoir/Savoir/ViewControllers/UpdateInfoViewController.h
+++ Savoir/Savoir/ViewControllers/UpdateInfoViewController.h
@@ -7,8 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+//#import "BaseViewController.h"
 
 
-@interface UpdateInfoViewController : UITableViewController <UITextFieldDelegate>
+@interface UpdateInfoViewController : UITableViewController
 
 @end
--- Savoir/Savoir/ViewControllers/UpdateInfoViewController.m
+++ Savoir/Savoir/ViewControllers/UpdateInfoViewController.m
@@ -8,26 +8,31 @@
 
 #import "UpdateInfoViewController.h"
 #import "AppDelegate.h"
+#import "PTKView.h"
 
-@interface UpdateInfoViewController ()
+
+@interface UpdateInfoViewController () <UITextFieldDelegate, PTKViewDelegate>
+
 @property (strong, nonatomic) IBOutlet UITableView *tableView;
 
 @property (strong, nonatomic) IBOutlet UITextField * txtFldFirstName;
 @property (strong, nonatomic) IBOutlet UITextField * txtFldLastName;
 @property (strong, nonatomic) IBOutlet UITextField * txtFldEmail;
 @property (strong, nonatomic) IBOutlet UITextField * txtFldPhone;
-@property (strong, nonatomic) IBOutlet UITextField * txtFldPaymentInfo;
 @property (strong, nonatomic) IBOutlet UITextField * txtFldFacebookProfile;
 
 @property (strong, nonatomic) IBOutlet UISlider * sldrTip;
 
 @property (strong, nonatomic) IBOutlet UILabel * lblSldrValue;
 
+@property (strong, nonatomic) IBOutlet PTKView * ptkViewPayInfo;
+
 @end
 
 @implementation UpdateInfoViewController
 
-- (void)viewDidLoad {
+- (void) viewDidLoad
+{
     [super viewDidLoad];
     AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication]delegate];
     [appDelegate.notificationUtils getSlidingMenuBarButtonSetupWith:self];
@@ -37,7 +42,6 @@
     [_txtFldLastName setDelegate:self];
     [_txtFldEmail setDelegate:self];
     [_txtFldPhone setDelegate:self];
-    [_txtFldPaymentInfo setDelegate:self];
     [_txtFldFacebookProfile setDelegate:self];
     
     //
@@ -45,7 +49,6 @@
     [_txtFldLastName setTextColor:[UIColor whiteColor]];
     [_txtFldEmail setTextColor:[UIColor whiteColor]];
     [_txtFldPhone setTextColor:[UIColor whiteColor]];
-    [_txtFldPaymentInfo setTextColor:[UIColor whiteColor]];
     [_txtFldFacebookProfile setTextColor:[UIColor whiteColor]];
     
     //
@@ -53,7 +56,6 @@
     [_txtFldLastName setKeyboardType:UIKeyboardTypeDefault];
     [_txtFldEmail setKeyboardType:UIKeyboardTypeEmailAddress];
     [_txtFldPhone setKeyboardType:UIKeyboardTypeNumbersAndPunctuation];
-    [_txtFldPaymentInfo setKeyboardType:UIKeyboardTypeDefault];
     [_txtFldFacebookProfile setKeyboardType:UIKeyboardTypeDefault];
     
     _sldrTip.minimumValue = 18;
@@ -63,16 +65,13 @@
     
     _lblSldrValue.text = [NSString stringWithFormat:@"%d%%", (int) _sldrTip.value];
     
+    [_ptkViewPayInfo setDelegate:self];
+    
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
-
-- (void) tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    cell.backgroundColor = [UIColor clearColor];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -101,9 +100,9 @@
 
 #pragma mark
 
-- (void) tableView:(UITableView *) tableView didSelectRowAtIndexPath:(NSIndexPath *) indexPath
+- (void) tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell * cell = [tableView cellForRowAtIndexPath:indexPath];
+    cell.backgroundColor = [UIColor clearColor];
     cell.selectionStyle = UITableViewCellSelectionStyleNone;
 }
 
